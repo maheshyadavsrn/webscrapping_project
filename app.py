5fbb47f18b1a43508537f6d9708331eead971961
Mahesh Yadav <maheshyadavsrn2022@gmail.com>
2023-07-21 00:12:08 +0530

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request,jsonify
+from pymongo.mongo_client import MongoClient
 from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
-
+import pymongo
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
@@ -72,14 +73,22 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+
+
+            client=pymongo.MongoClient("mongodb+srv://Mahesh:maheshyadav@cluster0.xynhc6w.mongodb.net/?retryWrites=true&w=majority")
+            db = client('review_scrap')
+            review_col=db['review_scrap_data']
+            review_col.insert_many(reviews)
+
+
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
-            logging.info(e)
-            return 'something is wrong'
+          logging.info(e)
+          return 'something is wrong'
     # return render_template('results.html')
 
     else:
-        return render_template('index.html')
+     return render_template('index.html')
 
 
 if __name__=="__main__":
